d1ec27f4f1e8dcabcfe8c6d1d7375e3529f9f328
Oskar Leirvåg <oskar.leir@gmail.com>
2018-10-03 14:37:45 +0200

    add program to build JAR artifact for easier use.

--- src/inf226/Program.java
+++ src/inf226/Program.java
@@ -8,4 +8,17 @@ package inf226;
  * @since 03.10.18
  */
 public class Program {
+    public static void main(String[] args) {
+        String[] port = {"localhost"};
+        switch (args[0]) {
+            case "s":
+            case "server":
+                Server.main(null);
+                break;
+            case "c":
+            case "client":
+                Client.main(port);
+                break;
+        }
+    }
 }
